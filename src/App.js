cabc7439126f7d55ca6653384188c9f9488d47d8
Rishi <rishisahu4456@gmail.com>
2022-10-06 13:42:50 +0530

    Added dark mode feature in navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import Navbar from './components/navbar/Navbar';
 import './App.css';
 import TextForm from './components/TextArea/TextArea';
+import React, {useState} from 'react'
 
 function App() {
+  const [mode, setmode] = useState('light')
+
+ const toggleMode=()=>{
+    if(mode==="light"){
+      setmode("dark")
+    }
+    else{
+      setmode("light")
+    }
+  }
+  
+
   return (
-   <> <Navbar title="Textutiles" about="About"/>
-   <div className="container"><TextForm/></div> </>
+    <>
+
+      <Navbar title="Textutiles" about="About"  mode={mode} toggleMode={toggleMode}/>
+      <div className="container"><TextForm/></div>
+
+    </>
   );
 }
 
---
+++ src/components/about/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function About(){
+    return(
+        <div className="container">
+            This is my about.
+        </div>
+    )
+}  
